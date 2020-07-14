b41f0ba62ec555514b2a9514171ab9601adfe425
plm <plm@nti.dk>
2020-07-14 10:50:39 +0200

    Implementing aircraft service.

--- FlightSimulator/FlightSimulator/Services/AirCraftService.cs
+++ FlightSimulator/FlightSimulator/Services/AirCraftService.cs
@@ -1,14 +1,28 @@
-﻿using System;
+﻿using FlightSimulator.Services.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FlightSimulator.Services
 {
-    public class AirCraftService
+    public interface IAirCraftService
     {
-        public AirCraftService()
+        List<Aircraft> Get();
+    }
+
+    public class AirCraftService : IAirCraftService
+    {
+        private readonly IStorageService storageService;
+
+        public AirCraftService(IStorageService storageService)
         {
+            this.storageService = storageService;
+        }
+
+        public List<Aircraft> Get()
+        {
+            return storageService.Aircraft;
         }
     }
 }
